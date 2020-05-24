57e5433c495982995ab4ffd027b20439e5d6a5cd
anegm98 <a.negm98.an@gmail.com>
2020-05-24 06:28:08 +0200

    detect motion

--- repReduce.cpp
+++ repReduce.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-Mat& ScanImageAndReduceC(Mat& I)
+Mat& ScanImageAndReduceC(Mat& I, Mat& I1)
 {
   // accept only char type matrices
   CV_Assert(I.depth() == CV_8U);
@@ -16,14 +16,19 @@ Mat& ScanImageAndReduceC(Mat& I)
   }
   int i,j;
   uchar* p;
+  uchar* p1;
   for( i = 0; i < nRows; ++i)
   {
       p = I.ptr<uchar>(i);
+      p1 = I1.ptr<uchar>(i);
       for ( j = 0; j < nCols; ++j)
       {
-        int si = (int)p[j];
-        std::string s = std::to_string(si);
-        std::cout << s << std::endl;
+        if ( (int)p[j] != (int)p1[j] ){
+          p[j] = (uchar)255;
+        }
+        else{
+          p[j] = (uchar)0;
+        }
       }
   }
   return I;
@@ -31,20 +36,28 @@ Mat& ScanImageAndReduceC(Mat& I)
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
+    if ( argc != 3 )
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    Mat image;
+    Mat image, image1;
     image = imread( argv[1], 1 );
+    image1 = imread( argv[2], 1 );
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    ScanImageAndReduceC(image);
+    if ( !image1.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    cvtColor(image, image, COLOR_BGR2GRAY);
+    cvtColor(image1, image1, COLOR_BGR2GRAY);
+    // image = ScanImageAndReduceC(image, image1);
+    threshold(abs(image - image1), image, 0, 255, 0);
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
     waitKey(0);
