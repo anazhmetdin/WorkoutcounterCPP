463bbec24e02e20e1f63a32a9d4c565e7e94f068
anegm98 <a.negm98.an@gmail.com>
2020-05-24 09:56:31 +0200

    calculating steps

--- repReduce.cpp
+++ repReduce.cpp
@@ -6,44 +6,35 @@ Mat& ScanImageAndReduceC(Mat& I, Mat& I1)
 {
   // accept only char type matrices
   CV_Assert(I.depth() == CV_8U);
-  int channels = I.channels();
   int nRows = I.rows;
-  int nCols = I.cols * channels;
-  if (I.isContinuous())
-  {
-      nCols *= nRows;
-      nRows = 1;
-  }
-  int i,j;
+  int nCols = I.cols;
+  int i,h,counter=0;
   uchar* p;
-  uchar* p1;
-  for( i = 0; i < nRows; ++i)
+  uchar* p1 = I1.ptr<uchar>(0);
+  for( h = 0; h < nCols*7/8; h += (int)nCols/8)
   {
-      p = I.ptr<uchar>(i);
-      p1 = I1.ptr<uchar>(i);
-      for ( j = 0; j < nCols; ++j)
-      {
-        if ( (int)p[j] != (int)p1[j] ){
-          p[j] = (uchar)255;
-        }
-        else{
-          p[j] = (uchar)0;
-        }
-      }
+    for( i = 0; i < nRows*5/6; i+=(int)nRows/6)
+    {
+      Mat temp = I(Rect(h, i, (int)nCols/8, (int)nRows/6));
+      p1[counter] = (int)mean(temp)[0];
+      counter++;
+    }
   }
-  return I;
+  return I1;
 }
 
 int main(int argc, char** argv )
 {
-    if ( argc != 3 )
+    if ( argc != 4 )
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    Mat image, image1;
+    Mat image, image1, image2;
+    Mat image3(6,8, CV_8U);
     image = imread( argv[1], 1 );
     image1 = imread( argv[2], 1 );
+    image2 = imread( argv[3], 1 );
     if ( !image.data )
     {
         printf("No image data \n");
@@ -54,12 +45,22 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
+    if ( !image2.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
     cvtColor(image, image, COLOR_BGR2GRAY);
     cvtColor(image1, image1, COLOR_BGR2GRAY);
-    // image = ScanImageAndReduceC(image, image1);
+    cvtColor(image2, image2, COLOR_BGR2GRAY);
     threshold(abs(image - image1), image, 0, 255, 0);
+    image3 = ScanImageAndReduceC(image, image3);
+    std::cout << image3 << std::endl;
+    threshold(abs(image2 - image1), image2, 0, 255, 0);
+    image3 = ScanImageAndReduceC(image2, image3);
+    std::cout << image3 << std::endl;
     namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    imshow("Display Image", image2);
     waitKey(0);
     return 0;
 }
